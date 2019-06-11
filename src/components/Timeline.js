3d7aee433f3997eef22ec0f2ee61312599b9d516
Erica Norris <norrise120@gmail.com>
2019-06-10 22:31:15 -0700

    Did soo many things

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,29 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
-  // Fill in your code here
-  return;
+const generateTimelineEventComponents = (events) => {
+  return events.map((event) => {
+    const { person, status, timeStamp } = event;
+    return (<TimelineEvent
+      key={person}
+      person={person}
+      status={status}
+      timeStamp={timeStamp}
+    />);
+  });
+};
+
+const Timeline = (props) => {
+  const { events } = props;
+
+  const eventComponents = generateTimelineEventComponents(events)
+
+  events.forEach(function(event) {
+
+  })
+  return (
+    <section>{eventComponents}</section>
+  );
 }
 
 export default Timeline;
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,15 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
+const TimelineEvent = (props) => {
   // Fill in your code here
-  return;
+  return (
+    <section>
+      <h3>{props.person}</h3>
+      <Timestamp time={props.time} />
+      <p>{props.status}</p>
+    </section>
+  );
 }
 
 export default TimelineEvent;
